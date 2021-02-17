f37c9b77b51e74dabb1a978721d3fc36ea7703c3
anthonysiu2000 <tonysiu123@yahoo.com>
2021-02-17 18:03:40 -0500

    added fx ids

--- src/view/SongLibController.java
+++ src/view/SongLibController.java
@@ -2,8 +2,27 @@
 
 package view;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
 public class SongLibController {
 
+	@FXML
+	ListView<String> listView;
+	
+	private ObservableList<String> obsList;
+	
+	public void start() {
+		// create an ObservableList
+		// from an ArrayList
+		obsList = FXCollections.observableArrayList(
+				"Giants",
+				"Patriots",
+				"Jaguars");
+				listView.setItems(obsList);
+	}
 }
 
 
